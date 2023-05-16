b2acfaa1a2dd5b8600d33043bc9963fd0a8af669
Bruno Masdeu <brudanmax@gmail.com>
2023-05-15 21:14:05 -0300

    Clase AstroMegaShip (todavia no dispara, se mueve de izquierda a derecha y no se pasa de la pantalla)

--- src/juego/AstroMegaShip.java
+++ src/juego/AstroMegaShip.java
@@ -1,8 +1,63 @@
 package juego;
 
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Point;
+import entorno.Entorno;
+
 public class AstroMegaShip {
-	AstroMegaShip(){
-		int x;
-		int y;
+	private int x;
+	private int y;
+	private int alto;
+	private int ancho;
+	private int velocidad;
+	private double angulo;
+	private Image imagen;
+
+	AstroMegaShip(int x, int y, int velocidad, double angulo, Image imagen) {
+		this.x = x;
+		this.y = y;
+		this.alto = 70; //Alto de la imagen de la nave
+		this.ancho = 68; //Ancho de la imagen de la nave
+		this.velocidad = 3;
+		this.angulo = angulo;
+		this.imagen = imagen;
 	}
+	
+	//Metodo para que se dibuje la nave en la pantalla
+	void dibujar(Entorno entorno) {
+		entorno.dibujarImagen(this.imagen, this.x , this.y , this.angulo);
+	}
+	
+	//Metodo para que la nave se dirija a la derecha
+	void moverDerecha() {
+		this.x = this.x + this.velocidad;
+	}
+	
+	//Metodo para que la nave se dirija a la izquierda
+	void moverIzquierda() {
+		this.x = this.x - this.velocidad;
+	}
+	
+	//Metodo para que de su posicion
+	Point posicion() {
+		Point centroNave = new Point(this.x, this.y);
+		return centroNave;
+	}
+	
+	//Rectangulo de la nave
+	Rectangle rectangulo() {
+	Rectangle centroNave = new Rectangle(this.x, this.y, this.ancho, this.alto);
+		return centroNave;
+	}
+	
+	//getters
+	int getX() {
+		return x;
+	}
+	
+	int getAncho() {
+		return ancho;
+	}
+
 }
--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -11,12 +11,16 @@ public class Juego extends InterfaceJuego {
 	private Entorno entorno;
 	private Image imagenDelRayo = Herramientas.cargarImagen("imagenes/rayo2.png");
 	private Rayo rayo;
+	private Image imagenAstroMS = Herramientas.cargarImagen("imagenes/nave3.png");
+	private AstroMegaShip astroMegaShip;
 
 	Juego() {
 		// Inicializa el objeto entorno
 		this.entorno = new Entorno(this, "Lost Galaxian - Grupo ... - v1", 800, 600);
 		// Rayo
-		this.rayo = new Rayo(500, 50, 4, Herramientas.radianes(90), imagenDelRayo);
+		this.rayo = new Rayo(500, 50, 4, Herramientas.radianes(180), imagenDelRayo);
+		// AstroMegaShip
+		this.astroMegaShip = new AstroMegaShip(400, 500, 2, Herramientas.radianes(0), imagenAstroMS);
 		//
 		this.entorno.iniciar();
 	}
@@ -27,6 +31,16 @@ public class Juego extends InterfaceJuego {
 			rayo.mover();
 			if (rayo.posicion() > 500)
 				rayo = null;
+		}
+		//if (astroMegaShip != null) {
+		this.astroMegaShip.dibujar(this.entorno);
+		if (this.entorno.estaPresionada(this.entorno.TECLA_DERECHA)
+			&& this.astroMegaShip.getX() + this.astroMegaShip.getAncho() / 2 < this.entorno.ancho()) {
+			this.astroMegaShip.moverDerecha();
+		}
+		if (this.entorno.estaPresionada(this.entorno.TECLA_IZQUIERDA)
+			&& this.astroMegaShip.getX() - this.astroMegaShip.getAncho() / 2 > 0) {
+			this.astroMegaShip.moverIzquierda();
 		}
 	}
 
